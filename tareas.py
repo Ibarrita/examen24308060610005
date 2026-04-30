4941aba9c728060ea941e0410e3962ddb3cccffc
José Ibarra <204079627+Ibarrita@users.noreply.github.com>
2026-04-29 22:37:15 -0400

    se cambio el nombre de la conexión

--- tareas.py
+++ tareas.py
@@ -13,7 +13,7 @@ class GestorTareas:
         try:
             self.cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
             self.cliente.admin.command('ping')
-            self.db = self.cliente['gestor_tareas']
+            self.db = self.cliente['24308060610005']
             self.tareas = self.db['tareas']
             self.usuarios = self.db['usuarios']
             
